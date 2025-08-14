e315d6aaf626d900a71740f7a771d84df4ee5eb2
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-08-14 18:16:03 -0400

    enhance authentication and user model with metadata and sales functionality

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,22 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client"
+import { betterAuth }    from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { PrismaClient }  from "@prisma/client"
 
-const prisma = new PrismaClient();
-export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql",
-  }),
+const prisma      = new PrismaClient()
+export const auth = betterAuth( {
+  database        : prismaAdapter( prisma, {
+    provider: "postgresql"
+  } ),
   emailAndPassword: {
-    enabled: true,
+    enabled: true
   },
-});
+  user            : {
+    additionalFields: {
+      metadata: {
+        type    : "string",
+        required: true,
+        input   : false
+      }
+    }
+  }
+} )
