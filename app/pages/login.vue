88f4679f37cc5b42dfa66f486d8dc346d88a5029
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-08-27 10:24:52 -0400

    refactor login component: enhance form handling with validation

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,25 +1,34 @@
 <script setup lang="ts">
 
 import { authClient } from "~~/lib/auth_client"
+import { registerRequestSchema } from "~~/modules/user/application/models/register_request"
+import { loginRequestSchema } from "~~/modules/user/application/models/login_request"
+import FInputGroup from "~/components/form/FInputGroup.vue"
+import FPassword from "~/components/form/FPassword.vue"
 
-const email = ref<string>( "" )
-const pass  = ref<string>( "" )
+const validationSchema = toTypedSchema( loginRequestSchema)
+const form             = useForm( {
+  validationSchema
+} )
+const loading          = ref( false )
+const router           = useRouter()
+const store            = useAuthStore()
 
-const onSubmit = async () => {
-  const { data, error } = await authClient.signIn.email( {
-    email   : email.value,
-    password: pass.value
-  } )
-  console.log( 'result: ',data, error )
-}
+const onSubmit         = form.handleSubmit( async ( values ) => {
+  loading.value = true
+  const result  = store.login( values )
+  console.log( "result: ", result )
+  loading.value = false
+  await router.replace( "/" )
+})
 </script>
 
 <template>
-  <div>
-    <InputText type="text" v-model="email"/>
-    <InputText type="text" v-model="pass"/>
+  <form @submit.prevent="onSubmit">
+    <f-input-group name="email" placeholder="Email" type="email" icon-text="pi pi-envelope"></f-input-group>
+    <f-password name="password" placeholder="Contraseña"></f-password>
     <Button label="Login" @click="onSubmit">Enter</Button>
-  </div>
+  </form>
 </template>
 
 <style scoped>
