25893e212c63710f7050c9155b364af14be9ebe6
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-09-05 12:28:17 -0400

    temp fix typo

--- app/middleware/check_admin.ts
+++ app/middleware/check_admin.ts
@@ -1,9 +1,9 @@
-export default defineNuxtRouteMiddleware( async ( to ) => {
- const user = useUser()
-  if(!user.can('MOD')){
-    throw createError( {
-      statusCode: 404,
-      message   : "Unauthorized"
-    } )
-  }
-} )
+// export default defineNuxtRouteMiddleware( async ( to ) => {
+//  const user = useUser()
+//   if(!user.can('MOD')){
+//     throw createError( {
+//       statusCode: 404,
+//       message   : "Unauthorized"
+//     } )
+//   }
+// } )
--- app/middleware/check_auth.ts
+++ app/middleware/check_auth.ts
@@ -1,9 +1,9 @@
-export default defineNuxtRouteMiddleware( async ( to ) => {
-  const { loggedIn } = useUserSession()
-  if ( !loggedIn.value ) {
-    throw createError( {
-      statusCode: 404,
-      message   : "Unauthorized"
-    } )
-  }
-} )
+// export default defineNuxtRouteMiddleware( async ( to ) => {
+//   const { loggedIn } = useUserSession()
+//   if ( !loggedIn.value ) {
+//     throw createError( {
+//       statusCode: 404,
+//       message   : "Unauthorized"
+//     } )
+//   }
+// } )
